4d40ccc09b587454a95b60268b80c5a541659cac
ParanormalVibe <foo@fennel.io>
2019-11-18 05:50:42 -0500

    refactor enum

---
+++ enums.h
@@ -0,0 +1,16 @@
+enum BiomeType {
+	Plains = 0,
+	Ocean = 1,
+	Hills = 2,
+	Woodlands = 3,
+	Jungle = 4,
+	Darkwoods = 5,
+	SnowForest = 6,
+	Mountains = 7,
+	Wetlands = 8,
+	Desert = 9,
+	Savannah = 10,
+	Snowlands = 0xb,
+	Deadlands = 0xc,
+	Firelands = 0xd
+};
--- main.cpp
+++ main.cpp
@@ -1,102 +1,63 @@
 #include "cwmods/cwmods.h"
-#include <string>
+#include "enums.h"
 
-int CalcBiomeType(long param_1, int x, int y)
-{
+bool IsInRange(int min, int max, int v) {
+	return (v >= min && v <= max);
+}
+int CalcMagicNumber(int x, int y) {
 	int v1;
-	int biomeType;
-
-	if ((x + 1U < 3) && (y + 1U < 3)) {
-		return 1;
-	}
-	biomeType = 0;
+	int magicNumber;
 	v1 = x + 0xb3f58 + y * 0x39;
 	v1 = v1 ^ v1 * 0x2000;
-
-	switch ((100000 - ((v1 * v1 * 0xec4d + 0x131071f) * v1 + 0xd208dd0d &
-		0x7fffffff) / 0x53e2) % 0x32) {
-	case 1:
-	case 2:
-	case 3:
-	case 4:
-	case 5:
-	case 6:
-	case 7:
-	case 8:
-	case 9:
-	case 10:
-	case 0xb:
-	case 0xc:
-	case 0xd:
-	case 0xe:
-	case 0xf:
-	case 0x10:
-	case 0x11:
-	case 0x12:
-	case 0x13:
-	case 0x14:
-	case 0x15:
-	case 0x16:
-	case 0x17:
-	case 0x18:
-	case 0x19:
-	case 0x1a:
-	case 0x1b:
-	case 0x1c:
-	case 0x1d:
-		return 1;
-	case 0x1e:
-	case 0x1f:
-		return 5;
-	case 0x20:
-	case 0x21:
-		return 2;
-	case 0x22:
-	case 0x23:
-		return 3;
-	case 0x24:
-	case 0x25:
-		return 4;
-	case 0x26:
-		return 0xd;
-	case 0x27:
-		return 6;
-	case 0x28:
-	case 0x29:
-		return 7;
-	case 0x2A:
-	case 0x2B:
-		return 8;
-	case 0x2C:
-	case 0x2D:
-		return 9;
-	case 0x2E:
-	case 0x2F:
-		return 10;
-	case 0x30:
-	case 0x31:
-		return 0xb;
-	case 0x32:
-		return 0xc;
-	case 0x33:
-	case 0x34:
-	case 0x35:
-		return 0xd;
+	magicNumber = (100000 - ((v1 * v1 * 0xec4d + 0x131071f) * v1 + 0xd208dd0d &
+		0x7fffffff) / 0x53e2) % 0x32;
+	return magicNumber;
+}
+BiomeType CalcBiomeType(long param_1, int x, int y)
+{
+	if ((x + 1U < 3) && (y + 1U < 3)) {
+		return Ocean;
 	}
-	return biomeType;
+	int magicNumber = CalcMagicNumber(x, y);
+	if (IsInRange(1, 10, magicNumber) || IsInRange(0xb, 0x1d, magicNumber))
+		return Ocean;
+	if (IsInRange(0x1e, 0x1f, magicNumber))
+		return Darkwoods;
+	if (IsInRange(0x20, 0x21, magicNumber))
+		return Hills;
+	if (IsInRange(0x22, 0x23, magicNumber))
+		return Woodlands;
+	if (IsInRange(0x24, 0x25, magicNumber))
+		return Jungle;
+	if (magicNumber == 0x26)
+		return Firelands;
+	if (magicNumber == 0x27)
+		return SnowForest;
+	if (IsInRange(0x28, 0x29, magicNumber))
+		return Mountains;
+	if (IsInRange(0x2A, 0x2B, magicNumber))
+		return Wetlands;
+	if (IsInRange(0x2C, 0x2D, magicNumber))
+		return Desert;
+	if (IsInRange(0x2E, 0x2F, magicNumber))
+		return Savannah;
+	if (IsInRange(0x30, 0x31, magicNumber))
+		return Snowlands;
+	if (magicNumber == 0x32)
+		return Deadlands;
+	if (IsInRange(0x33, 0x35, magicNumber)) {
+		return Firelands;
+	}
+	return Plains;
 }
-
 void OverwriteBiomeFunction() {
-	auto funAddr = &CalcBiomeType;
-	WriteFarJMP(CWOffset(0x2B9080), reinterpret_cast<void*&>(funAddr));
+	WriteFarJMP(CWOffset(0x2B9080), (void*)((int*)&CalcBiomeType));
 }
-
 class BetterBiomes : GenericMod {
 	virtual void Initialize() override {
 		OverwriteBiomeFunction();
 	}
 };
-
 EXPORT BetterBiomes* MakeMod() {
 	return new BetterBiomes();
 }
